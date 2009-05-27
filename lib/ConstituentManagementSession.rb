be0847d9644c2a9c13d1724dded573b400cf211b
Brandon Wiley <brandon@blanu.net>
2009-05-27 12:13:00 -0500

    Fixed an accidental regresion of the code version

--- lib/ConstituentManagementSession.rb
+++ lib/ConstituentManagementSession.rb
@@ -30,56 +30,56 @@ class ConstituentManagementSession < ConvioSession
       @url=@@secure_domain+'/site/SRConsAPI'
     end
 
-    @defaultParams[:login_name]=@@login_name
-    @defaultParams[:login_password]=@@login_password
+    @defaultParams['login_name']=@@login_name
+    @defaultParams['login_password']=@@login_password
   end
 
   # This method creates a new user record and returns the record's unique identifier.
   def create(primary_email, add_center_ids=nil, add_group_ids=nil, add_interest_ids=nil, source=nil, no_welcome=nil)
     params=@defaultParams.clone
-    params[:method]="create"
+    params['method']="create"
     
     if primary_email
-      params[:primary_email]=primary_email
+      params['primary_email']=primary_email
     else
       puts "primary_email is required"
       return nil
     end
       
     if add_center_ids
-      params[:add_center_ids]=add_center_ids
+      params['add_center_ids']=add_center_ids
     end
     if remove_center_ids
-      params[:remove_center_ids]=remove_center_ids
+      params['remove_center_ids']=remove_center_ids
     end
     
     if add_group_ids
-      params[:add_group_ids]=add_group_ids
+      params['add_group_ids']=add_group_ids
     end
     if remove_group_ids
-      params[:add_remove_ids]=add_remove_ids
+      params['add_remove_ids']=add_remove_ids
     end
     
     if add_group_ids
-      params[:add_group_ids]=add_group_ids
+      params['add_group_ids']=add_group_ids
     end
     if remove_group_ids
-      params[:add_remove_ids]=add_remove_ids
+      params['add_remove_ids']=add_remove_ids
     end
     
     if add_interest_ids
-      params[:add_interest_ids]=add_interest_ids
+      params['add_interest_ids']=add_interest_ids
     end
     if remove_interest_ids
-      params[:add_interest_ids]=add_interest_ids
+      params['add_interest_ids']=add_interest_ids
     end
     
     if source
-      params[:source]=source
+      params['source']=source
     end
     
     if no_welcome
-      params[:no_welcome]=no_welcome
+      params['no_welcome']=no_welcome
     end
     
     result=convio_api_call(@url, params)
@@ -90,45 +90,45 @@ class ConstituentManagementSession < ConvioSession
   # This method updates an existing user record and returns the record's unique identifier.
   def update(cons_id=nil, member_id=nil, primary_email=nil, add_center_ids=nil, add_group_ids=nil, add_interest_ids=nil, remove_center_ids=nil, remove_group_ids=nil, remove_interest_ids=nil)
     params=@defaultParams.clone
-    params[:method]="update"
+    params['method']="update"
     
     if cons_id
-      params[:cons_id]=cons_id
+      params['cons_id']=cons_id.to_s
     elsif member_id
-      params[:member_id]=member_id
+      params['member_id']=member_id
     elsif primary_email
-      params[:primary_email]=primary_email
+      params['primary_email']=primary_email
     else
       puts "One of cons_id, member_id, or primary_email is required"
       return nil
     end
       
     if add_center_ids
-      params[:add_center_ids]=add_center_ids
+      params['add_center_ids']=add_center_ids
     end
     if remove_center_ids
-      params[:remove_center_ids]=remove_center_ids
+      params['remove_center_ids']=remove_center_ids
     end
     
     if add_group_ids
-      params[:add_group_ids]=add_group_ids
+      params['add_group_ids']=add_group_ids
     end
     if remove_group_ids
-      params[:add_remove_ids]=add_remove_ids
+      params['add_remove_ids']=add_remove_ids
     end
     
     if add_group_ids
-      params[:add_group_ids]=add_group_ids
+      params['add_group_ids']=add_group_ids
     end
     if remove_group_ids
-      params[:add_remove_ids]=add_remove_ids
+      params['add_remove_ids']=add_remove_ids
     end
     
     if add_interest_ids
-      params[:add_interest_ids]=add_interest_ids
+      params['add_interest_ids']=add_interest_ids
     end
     if remove_interest_ids
-      params[:add_interest_ids]=add_interest_ids
+      params['add_interest_ids']=add_interest_ids
     end
     
     result=convio_api_call(@url, params)
@@ -139,53 +139,53 @@ class ConstituentManagementSession < ConvioSession
   # This method provides a convenience wrapper around the Convio Client create and update APIs. When called, this method will first try to locate* and update an existing record, and if no existing record can be found, will create a new record. See the create and update documentation for further detailed discussion of those APIs.
   def createOrUpdate(cons_id=nil, member_id=nil, primary_email=nil, add_center_ids=nil, add_group_ids=nil, add_interest_ids=nil, remove_center_ids=nil, remove_group_ids=nil, remove_interest_ids=nil, source=nil, no_welcome=nil)
     params=@defaultParams.clone
-    params[:method]="createOrUpdate"
+    params['method']="createOrUpdate"
     
     if cons_id
-      params[:cons_id]=cons_id
+      params['cons_id']=cons_id.to_s
     elsif member_id
-      params[:member_id]=member_id
+      params['member_id']=member_id
     elsif primary_email
-      params[:primary_email]=primary_email
+      params['primary_email']=primary_email
     else
       puts "One of cons_id, member_id, or primary_email is required"
       return nil
     end
       
     if add_center_ids
-      params[:add_center_ids]=add_center_ids
+      params['add_center_ids']=add_center_ids
     end
     if remove_center_ids
-      params[:remove_center_ids]=remove_center_ids
+      params['remove_center_ids']=remove_center_ids
     end
     
     if add_group_ids
-      params[:add_group_ids]=add_group_ids
+      params['add_group_ids']=add_group_ids
     end
     if remove_group_ids
-      params[:add_remove_ids]=add_remove_ids
+      params['add_remove_ids']=add_remove_ids
     end
     
     if add_group_ids
-      params[:add_group_ids]=add_group_ids
+      params['add_group_ids']=add_group_ids
     end
     if remove_group_ids
-      params[:add_remove_ids]=add_remove_ids
+      params['add_remove_ids']=add_remove_ids
     end
     
     if add_interest_ids
-      params[:add_interest_ids]=add_interest_ids
+      params['add_interest_ids']=add_interest_ids
     end
     if remove_interest_ids
-      params[:add_interest_ids]=add_interest_ids
+      params['add_interest_ids']=add_interest_ids
     end
     
     if source
-      params[:source]=source
+      params['source']=source
     end
     
     if no_welcome
-      params[:no_welcome]=no_welcome
+      params['no_welcome']=no_welcome
     end
     
     result=convio_api_call(@url, params)
@@ -201,10 +201,10 @@ class ConstituentManagementSession < ConvioSession
   # This method returns a list of the possible values for a constituent record field.
   def listUserFieldChoices(field)
     params=@defaultParams.clone
-    params[:method]="listUserFieldChoices"
+    params['method']="listUserFieldChoices"
 
     if field
-      params[:field]=field
+      params['field']=field
     else
       puts "field is required"
       return nil
@@ -218,21 +218,21 @@ class ConstituentManagementSession < ConvioSession
   # This method returns the constituents in a group when called from another application or server.  It is only available when called using an administrator account.
   def getGroupMembers(group_id, max_number=nil, fields=nil)
     params=@defaultParams.clone
-    params[:method]="getGroupMembers"
+    params['method']="getGroupMembers"
 
     if group_id
-      params[:group_id]=group_id
+      params['group_id']=group_id
     else
       puts "field is required"
       return nil
     end
     
     if max_number
-      params[:max_number]=max_number
+      params['max_number']=max_number
     end
     
     if fields
-      params[:fields]=fields
+      params['fields']=fields
     end
     
     result=convio_api_call(@url, params)
@@ -245,21 +245,21 @@ class ConstituentManagementSession < ConvioSession
     puts "getUser(#{cons_id}, #{member_id}, #{primary_email}, #{fields})"
     
     params=@defaultParams.clone
-    params[:method]="getUser"
+    params['method']="getUser"
     
     if cons_id
-      params[:cons_id]=cons_id
+      params['cons_id']=cons_id.to_s
     elsif member_id
-      params[:member_id]=member_id
+      params['member_id']=member_id
     elsif primary_email
-      params[:primary_email]=primary_email
+      params['primary_email']=primary_email
     else
       puts "One of cons_id, member_id, or primary_email is required"
       return
     end
       
     if fields
-      params[:fields]=fields
+      params['fields']=fields
     end
     
     result=convio_api_call(@url, params)
@@ -270,14 +270,14 @@ class ConstituentManagementSession < ConvioSession
   # This method returns the groups that a constituent record is in.
   def getUserGroups(cons_id=nil, member_id=nil, primary_email=nil)
     params=@defaultParams.clone
-    params[:method]="getUserGroups"
+    params['method']="getUserGroups"
     
     if cons_id
-      params[:cons_id]=cons_id
+      params['cons_id']=cons_id.to_s
     elsif member_id
-      params[:member_id]=member_id
+      params['member_id']=member_id
     elsif primary_email
-      params[:primary_email]=primary_email
+      params['primary_email']=primary_email
     else
       puts "One of cons_id, member_id, or primary_email is required"
       return
@@ -290,14 +290,14 @@ class ConstituentManagementSession < ConvioSession
   
   def getUserInterests(cons_id=nil, member_id=nil, primary_email=nil)
     params=@defaultParams.clone
-    params[:method]="getUserInterests"
+    params['method']="getUserInterests"
     
     if cons_id
-      params[:cons_id]=cons_id
+      params['cons_id']=cons_id.to_s
     elsif member_id
-      params[:member_id]=member_id
+      params['member_id']=member_id
     elsif primary_email
-      params[:primary_email]=primary_email
+      params['primary_email']=primary_email
     else
       puts "One of cons_id, member_id, or primary_email is required"
       return
